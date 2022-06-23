8cd9bc8bcf38d99b3ba429914b2399ade88849d5
al20ov <nicolas.antauf@gmail.com>
2022-06-22 19:12:14 -0700

    [ADD]: perspective and rotating cube example

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ import { OBJLoader } from "@loaders.gl/obj";
 import PicoGL from "picogl";
 import { mat4 } from "gl-matrix";
 
-const obj = await load("assets/models/weirdplane.obj", OBJLoader);
-
-console.log([...obj.attributes.POSITION.value]);
+const obj = await load("assets/models/rotatedcube.obj", OBJLoader);
 
 const canvas = document.getElementById("canvas");
 canvas.width = 1600;
 canvas.height = 900;
 
 const gl = canvas.getContext("webgl2");
+
+gl.enable(gl.DEPTH_TEST);
 
 const app = PicoGL.createApp(canvas)
   .clearColor(0, 0, 0, 1);
@@ -32,19 +32,39 @@ let image = new Image();
 
 let angle = 0;
 
+function getMVP() {
+  const model = mat4.create();
+  // mat4.fromZRotation(model, angle);
+  mat4.fromScaling(model, [0.5, 0.5, 0.5])
+  mat4.rotateY(model, model, angle);
+  mat4.rotateX(model, model, angle);
+  angle += 0.01;
+
+  const view = mat4.create();
+  mat4.fromTranslation(view, [0, 0, -1.5]);
+
+  const projection = mat4.create();
+  mat4.perspective(projection, 90 * (Math.PI / 180), 16.0 / 9.0, 0.01, 100);
+  return [
+    model,
+    view,
+    projection,
+  ];
+}
+
 function draw() {
   const texture = app.createTexture2D(image, { flipY: true });
-  let rotation = mat4.create();
-  mat4.fromZRotation(rotation, angle);
+  const [model, view, projection] = getMVP();
   const drawCall = app.createDrawCall(program, triangleArray)
-    .uniform("rotation", rotation)
+    .uniform("model", model)
+    .uniform("view", view)
+    .uniform("projection", projection)
     .texture("tex", texture);
 
   app.clear();
 
   drawCall.draw();
 
-  angle += 0.02;
   setTimeout(draw, 16);
 }
 
