bcb41f4323d78baff9252e47fefe7d90b8f6e374
al20ov <nicolas.antauf@gmail.com>
2022-06-22 22:05:20 -0700

    [ADD]: ambient and diffuse shading (flat)

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { OBJLoader } from "@loaders.gl/obj";
 import PicoGL from "picogl";
 import { mat4 } from "gl-matrix";
 
-const obj = await load("assets/models/rotatedcube.obj", OBJLoader);
+const obj = await load("assets/models/sphere.obj", OBJLoader);
 
 const canvas = document.getElementById("canvas");
 canvas.width = 1600;
@@ -25,8 +25,11 @@ const program = app.createProgram(vSource, fSource);
 // deno-fmt-ignore
 const positions = app.createVertexBuffer(PicoGL.FLOAT, 3, obj.attributes.POSITION.value);
 
+const normals = app.createVertexBuffer(PicoGL.FLOAT, 3, obj.attributes.NORMAL.value);
+
 const triangleArray = app.createVertexArray()
-  .vertexAttributeBuffer(0, positions);
+  .vertexAttributeBuffer(0, positions)
+  .vertexAttributeBuffer(1, normals);
 
 let image = new Image();
 
@@ -35,13 +38,15 @@ let angle = 0;
 function getMVP() {
   const model = mat4.create();
   // mat4.fromZRotation(model, angle);
-  mat4.fromScaling(model, [0.5, 0.5, 0.5])
-  mat4.rotateY(model, model, angle);
-  mat4.rotateX(model, model, angle);
-  angle += 0.01;
+  mat4.fromScaling(model, [1.0, 1.0, 1.0])
+  // mat4.rotateY(model, model, angle);
+  // mat4.rotateX(model, model, angle);
+  angle += 0.04;
 
   const view = mat4.create();
-  mat4.fromTranslation(view, [0, 0, -1.5]);
+  mat4.translate(view, view, [0, 0, -1.5]);
+  // mat4.rotateX(view, view, angle / 2.0);
+  mat4.rotateY(view, view, angle / 2.0);
 
   const projection = mat4.create();
   mat4.perspective(projection, 90 * (Math.PI / 180), 16.0 / 9.0, 0.01, 100);
