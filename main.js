4fb49e55f1cc8772e8db81db643c0bae1f13b783
al20ov <nicolas.antauf@gmail.com>
2022-06-23 21:59:42 -0700

    [ADD]: modified brdf shader to accomodate directional lights (works by removing light distance and attenuation)

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { OBJLoader } from "@loaders.gl/obj";
 import PicoGL from "picogl";
 import { mat4, vec3 } from "gl-matrix";
 
-const obj = await load("assets/models/monke.obj", OBJLoader);
+const obj = await load("assets/models/extremesphere.obj", OBJLoader);
 
 const canvas = document.getElementById("canvas");
 canvas.width = 1600;
@@ -42,17 +42,19 @@ let angle = 0;
 
 function getMVP() {
   // const camPos = vec3.fromValues(Math.sin(angle) * 10.0, Math.cos(angle) * 10.0, 10.0);
-  const camPos = vec3.fromValues(Math.sin(angle / 10.0) * 10.0, 0.0, 7.0);
+  // const camPos = vec3.fromValues(Math.sin(angle / 10.0) * 10.0, 0.0, 5.0);
+  const camPos = vec3.fromValues(0.0, 0.0, 2.0);
+
   const model = mat4.create();
   mat4.identity(model);
-  mat4.fromTranslation(model, vec3.fromValues(1.0, 0.0, 0.0));
+  mat4.fromTranslation(model, vec3.fromValues(0.0, 0.0, 0.0));
   angle += Math.PI / 128;
 
   const view = mat4.create();
-  mat4.lookAt(view, camPos, [0.0, 0.0, 0.0], [0.0, 1.0, 0.0])
+  mat4.lookAt(view, camPos, [0.0, 0.0, 0.0], [0.0, 1.0, 0.0]);
 
   const projection = mat4.create();
-  mat4.perspective(projection, 20 * (Math.PI / 180), 16.0 / 9.0, 0.01, 100);
+  mat4.perspective(projection, 75 * (Math.PI / 180), 16.0 / 9.0, 0.01, 100);
   return [
     camPos,
     model,
@@ -72,21 +74,26 @@ function draw() {
     .uniform("camPos", camPos)
     .uniform("albedo", [0.1, 0.5, 0.0])
     .uniform("metallic", 0.0)
-    .uniform("roughness", 0.6)
+    .uniform("roughness", 0.3)
     .uniform("ao", 0.6)
     .uniform("lightPositions[0]", [
-      Math.cos(angle) * 10.0,
-      Math.sin(angle) * 10.0,
-      8.0,
+      // With directional lights (like the sun), since the light source is
+      // "infinitely" far away, distance is irrelevant and what matters is the
+      // orientation of the light source. The light source can be as "close" or
+      // far away as you want, it won't change a thing as long as it's not at
+      // [0, 0, 0]
+      Math.cos(angle) * 0.1,
+      Math.sin(angle) * 0.1,
+      0.0,
     ]);
 
   // .texture("tex", texture);
 
   app.clear();
 
-  drawCall.draw();
-  mat4.translate(model, model, vec3.fromValues(-2.0, 0.0, 0.0));
-  drawCall.uniform("roughness", 0.06).uniform("albedo", [1.0, 0.0, 0.2])
+  // drawCall.draw();
+  // mat4.translate(model, model, vec3.fromValues(-2.0, 0.0, 0.0));
+  // drawCall.uniform("roughness", 0.06).uniform("albedo", [1.0, 0.0, 0.2])
   drawCall.draw();
 
   setTimeout(draw, 16.67);
