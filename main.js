15deac0e08014f93c7827d8c1cf022b7484784f3
al20ov <nicolas.antauf@gmail.com>
2022-06-23 19:54:46 -0700

    [ADD]: monke test with accurate pbr

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { OBJLoader } from "@loaders.gl/obj";
 import PicoGL from "picogl";
 import { mat4, vec3 } from "gl-matrix";
 
-const obj = await load("assets/models/simplesphere.obj", OBJLoader);
+const obj = await load("assets/models/monke.obj", OBJLoader);
 
 const canvas = document.getElementById("canvas");
 canvas.width = 1600;
@@ -16,7 +16,7 @@ gl.enable(gl.DEPTH_TEST);
 gl.depthFunc(gl.LESS);
 
 const app = PicoGL.createApp(canvas)
-  .clearColor(0, 0, 0, 1);
+  .clearColor(0.0, 0.0, 0.0, 1);
 
 const vSource = document.getElementById("vertex-shader").text.trim();
 const fSource = document.getElementById("fragment-shader").text.trim();
@@ -41,23 +41,20 @@ const triangleArray = app.createVertexArray()
 let angle = 0;
 
 function getMVP() {
+  // const camPos = vec3.fromValues(Math.sin(angle) * 10.0, Math.cos(angle) * 10.0, 10.0);
+  const camPos = vec3.fromValues(Math.sin(angle / 10.0) * 10.0, 0.0, 7.0);
   const model = mat4.create();
   mat4.identity(model);
-  // mat4.fromZRotation(model, angle);
-  // mat4.fromScaling(model, [1.0, 1.0, 1.0]);
-  // mat4.rotateZ(model, model, angle);
-  // mat4.rotateX(model, model, angle);
-  angle += 0.02;
+  mat4.fromTranslation(model, vec3.fromValues(1.0, 0.0, 0.0));
+  angle += Math.PI / 128;
 
   const view = mat4.create();
-  mat4.identity(view);
-  mat4.translate(view, view, [0, 0, -2.5]);
-  // mat4.rotateX(view, view, angle)
-  // mat4.rotateY(view, view, angle)
+  mat4.lookAt(view, camPos, [0.0, 0.0, 0.0], [0.0, 1.0, 0.0])
 
   const projection = mat4.create();
-  mat4.perspective(projection, 90 * (Math.PI / 180), 16.0 / 9.0, 0.01, 100);
+  mat4.perspective(projection, 20 * (Math.PI / 180), 16.0 / 9.0, 0.01, 100);
   return [
+    camPos,
     model,
     view,
     projection,
@@ -66,25 +63,33 @@ function getMVP() {
 
 function draw() {
   // const texture = app.createTexture2D(image, { flipY: true });
-  const [model, view, projection] = getMVP();
+  const [camPos, model, view, projection] = getMVP();
+
   const drawCall = app.createDrawCall(program, triangleArray)
     .uniform("model", model)
     .uniform("view", view)
     .uniform("projection", projection)
-    .uniform("camPos", vec3.fromValues(view[12], view[13], -view[14]))
-    .uniform("albedo", [0.5, 0.0, 0.0])
+    .uniform("camPos", camPos)
+    .uniform("albedo", [0.1, 0.5, 0.0])
     .uniform("metallic", 0.0)
-    .uniform("roughness", 0.4)
-    .uniform("ao", 0.3)
-    .uniform("lightPositions[0]", [10.0, 10.0, 10.0]);
+    .uniform("roughness", 0.6)
+    .uniform("ao", 0.6)
+    .uniform("lightPositions[0]", [
+      Math.cos(angle) * 10.0,
+      Math.sin(angle) * 10.0,
+      8.0,
+    ]);
 
   // .texture("tex", texture);
 
   app.clear();
 
   drawCall.draw();
+  mat4.translate(model, model, vec3.fromValues(-2.0, 0.0, 0.0));
+  drawCall.uniform("roughness", 0.06).uniform("albedo", [1.0, 0.0, 0.2])
+  drawCall.draw();
 
-  setTimeout(draw, 16);
+  setTimeout(draw, 16.67);
 }
 
 // image.onload = () => {
